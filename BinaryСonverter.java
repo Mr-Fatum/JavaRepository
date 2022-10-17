de92def7902a801bb5ae267a86101a9623ba557a
Mr-Fatum <114167306+Mr-Fatum@users.noreply.github.com>
2022-10-17 23:19:49 +0500

    Update BinaryСonverter.java

--- BinaryСonverter.java
+++ BinaryСonverter.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -50,55 +51,40 @@ public class BinaryСonverter {
                 break;
             try {
                 byteInput = scanner.nextByte();
-            } catch (Exception egnored) {
-            }
-            try {
                 shortInput = scanner.nextShort();
-            } catch (Exception egnored) {
-            }
-            try {
                 integerInput = scanner.nextInt();
-            } catch (Exception egnored) {
-            }
-            try {
                 longInput = scanner.nextLong();
-            } catch (Exception egnored) {
-            }
-            try {
                 floatInput = scanner.nextFloat();
-            } catch (Exception egnored) {
-            }
-            try {
                 doubleInput = scanner.nextDouble();
-            } catch (Exception egnored) {
+            } catch (InputMismatchExeption IME) {
+                System.out.println(IME.getMessage() + "Something went wrong");
             }
-            
         }
         switch (a) {
             case 1:
-                String s1 = String.format(Integer.toBinaryString(byteInput & 0xFF));
-                System.out.println(s1);
+                String byteString = String.format(Integer.toBinaryString(byteInput & 0xFF));
+                System.out.println(byteString);
                 break;
             case 2:
-                String s2 = String.format(Integer.toBinaryString(shortInput & 0xFF));
-                System.out.println(s2);
+                String shortString = String.format(Integer.toBinaryString(shortInput & 0xFF));
+                System.out.println(shortString);
                 break;
             case 3:
-                String s3 = Integer.toBinaryString(integerInput);
-                System.out.println(s3);
+                String integerString = Integer.toBinaryString(integerInput);
+                System.out.println(integerString);
                 break;
             case 4:
-                String s4 = String.format(Long.toBinaryString(longInput & 0xFF));
-                System.out.println(s4);
+                String longString = String.format(Long.toBinaryString(longInput & 0xFF));
+                System.out.println(longString);
                 break;
             case 5:
                 int str = Float.floatToIntBits(floatInput);
-                String s5 = Integer.toBinaryString(str);
-                System.out.println(s5);
+                String floatString = Integer.toBinaryString(str);
+                System.out.println(floatString);
                 break;
             case 6:
-                String s6 = String.format(Long.toBinaryString(Double.doubleToRawLongBits(doubleInput)));
-                System.out.println(s6);
+                String doubleString = String.format(Long.toBinaryString(Double.doubleToRawLongBits(doubleInput)));
+                System.out.println(doubleString);
                 break;
             default:
                 System.out.println("Ошибка, перезапустите код и введите число в выбранном типе данных");
